6230b00c3ff663b5a43bd731c367f708d5911797
akram120 <44569624+akram120@users.noreply.github.com>
2025-05-31 18:02:14 +0300

    Add files via upload

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,1 +1,13 @@
+var mongo = require('mongoose');
 
+
+
+console.log("try connect");
+var url = "mongodb://172.16.151.33:27017/alnoamanApi";
+mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  },function(err, db) {
+    if (err) throw err;
+    else
+    console.log("Database Connected");
+    
+   // db.close();
+  });
